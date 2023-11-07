4305f1fdf9b455d56993ee8787ec9e83acd71817
Artur Artemij Monsch <60860160+a-monsch@users.noreply.github.com>
2023-11-07 13:17:53 +0100

    Dev addition of individual triggerbit information (#33)
    * Adding producers for the individual bit quantities of a trigger, adding DZ information to NanoAOD
    
    * Added (back) accidentally removed single trigger from config
    
    * Adding trg_bit quantities with additional trg_ptcut
    
    * Adjusting config to muon/embeddingselection correctionlib split

--- tau_embedding_settings.py
+++ tau_embedding_settings.py
@@ -156,8 +156,8 @@ def setup_embedding(configuration: Configuration, scopes: List[str]):
         {
             "embedding_selection_sf_file": EraModifier(
                 {
-                    "2016preVFP": "data/embedding/muon_2016preVFPUL.json.gz",
-                    "2016postVFP": "data/embedding/muon_2016postVFPUL.json.gz",
+                    "2016preVFP": "data/embedding/embeddingselection_2016preVFPUL.json.gz",
+                    "2016postVFP": "data/embedding/embeddingselection_2016postVFPUL.json.gz",
                     "2017": "data/embedding/muon_2017UL.json.gz",
                     "2018": "data/embedding/muon_2018UL.json.gz",
                 }
