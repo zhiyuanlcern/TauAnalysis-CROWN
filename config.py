1eb92b225d28a9cd70496e8d7e422b9438ab051a
zhiyuanlcern <zhiyuan.li@cern.ch>
2025-02-28 11:21:49 +0800

    fix bug for recoil correction, using only jets pass lepton veto

--- config.py
+++ config.py
@@ -647,7 +647,9 @@ def build_config(
                     "2017": "data/recoil_corrections/Type1_PuppiMET_2017.root",
                     "2018": "data/recoil_corrections/Type1_PuppiMET_2018.root",
                     "2022EE": "data/jsonpog-integration/hleprare/DYandRecoilCorrlib/DY_pTll_recoil_corrections_2022preEE_v2.json.gz", 
-                    "2022postEE": "data/jsonpog-integration/hleprare/DYandRecoilCorrlib/DY_pTll_recoil_corrections_2022postEE_v2.json.gz",
+                    # "2022postEE": "data/jsonpog-integration/hleprare/DYandRecoilCorrlib/DY_pTll_recoil_corrections_2022postEE_v2.json.gz",
+                    "2022postEE": "data/jsonpog-integration/hleprare/RecoilCorrlib/Recoil_corrections_2022postEE_v2.json.gz",
+
                     "2023": "data/jsonpog-integration/hleprare/DYandRecoilCorrlib/DY_pTll_recoil_corrections_2023preBPix_v2.json.gz", 
                     "2023BPix": "data/jsonpog-integration/hleprare/DYandRecoilCorrlib/DY_pTll_recoil_corrections_2023postBPix_v2.json.gz", 
                 }
@@ -686,10 +688,10 @@ def build_config(
                     "2016postVFP": True,
                     "2017": True,
                     "2018": True,
-                    "2022EE": True,
+                    "2022EE": False,
                     "2022postEE": True,
-                    "2023": True,
-                    "2023BPix": True,
+                    "2023": False,
+                    "2023BPix": False,
                     
                 },
                 default=False,
