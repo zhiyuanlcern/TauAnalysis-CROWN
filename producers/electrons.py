3f7409970fb449bcb62e19fff9cfdab86bfe226d
zhiyuanl <zhiyuanl@cern.ch>
2025-08-28 10:19:28 +0200

    update for electron smearing and scaling

--- producers/electrons.py
+++ producers/electrons.py
@@ -35,7 +35,7 @@ ElectronPtCorrectionScaling = Producer(
 
 ElectronPtCorrectionSmearing = Producer(
     name="ElectronPtCorrectionSmearing",
-    call='physicsobject::electron::PtCorrection_smearing({df}, {output}, "{electron_SS_file}", "{electron_SS_smear_name}", {input})',
+    call='physicsobject::electron::PtCorrection_smearing({df}, {output}, "{electron_SS_file}", "{electron_SS_smear_name}", "{Smear_variation}", {input})',
     input=[
         nanoAOD.Electron_pt,
         nanoAOD.Electron_r9,
